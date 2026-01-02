ca9248f372270ae9e5bf6db022f81bfffee6a397
adolph0721 <garful.c@gmail.com>
2026-01-02 11:19:22 +0800

    Fix ESPHome external component folder structure

--- components/jsn_sensor/sensor.py
+++ components/jsn_sensor/sensor.py
@@ -4,15 +4,19 @@ from esphome.components import sensor, uart
 
 CONF_UART_ID = "uart_id"
 
-# C++ class 與 namespace
 jsn_ns = cg.esphome_ns.namespace("jsn_sensor")
 JSNSensor = jsn_ns.class_("JSNSensor", cg.PollingComponent, sensor.Sensor)
 
-# 新版 ESPHome schema
 CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
     cv.Required(sensor.CONF_ID): cv.declare_id(JSNSensor),
-})
+    cv.Optional(sensor.CONF_NAME): cv.string,
+    cv.Optional(sensor.CONF_UNIT_OF_MEASUREMENT): cv.string,
+    cv.Optional(sensor.CONF_ICON): cv.icon,
+    cv.Optional(sensor.CONF_DEVICE_CLASS): sensor.DEVICE_CLASSES_SCHEMA,
+    cv.Optional(sensor.CONF_STATE_CLASS): sensor.STATE_CLASSES_SCHEMA,
+    cv.Optional(sensor.CONF_ACCURACY_DECIMALS): cv.positive_int,
+}).extend(sensor.SENSOR_SCHEMA)  # 把 sensor 基本選項加入 schema
 
 async def to_code(config):
     uart_var = await cg.get_variable(config[CONF_UART_ID])
