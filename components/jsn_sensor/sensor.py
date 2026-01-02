a23ab684d8b3ed8080844074ec41eb6e7fcddb99
adolph0721 <garful.c@gmail.com>
2026-01-02 10:08:07 +0800

    Fix ESPHome external component folder structure

--- components/jsn_sensor/sensor.py
+++ components/jsn_sensor/sensor.py
@@ -1,16 +1,18 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, uart
-from esphome import pins
 
+# 告訴 ESPHome namespace
 jsn_sensor_ns = cg.esphome_ns.namespace("jsn_sensor")
 JSNSensor = jsn_sensor_ns.class_("JSNSensor", cg.PollingComponent, sensor.Sensor)
 
-PLATFORM_SCHEMA = cv.PLATFORM_SCHEMA.extend({
+# 新版 schema 定義方式
+CONFIG_SCHEMA = cv.Schema({
     cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
-}).extend(sensor.SENSOR_SCHEMA.schema)
+    cv.GenerateID(): cv.declare_id(JSNSensor),
+}).extend(sensor.SENSOR_SCHEMA)
 
 async def to_code(config):
     uart_ = await cg.get_variable(config["uart_id"])
-    var = cg.new_Pvariable(config["name"], uart_)
+    var = cg.new_Pvariable(config[cv.GenerateID()], uart_)
     await sensor.register_sensor(var, config)
