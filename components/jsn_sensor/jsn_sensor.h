b264b74b7d05a088f9783af9e822bfa4b91c6882
adolph0721 <garful.c@gmail.com>
2026-01-02 11:48:31 +0800

    Fix ESPHome external component folder structure

--- components/jsn_sensor/jsn_sensor.h
+++ components/jsn_sensor/jsn_sensor.h
@@ -1,42 +1,46 @@
 #pragma once
 
-#include "esphome.h"
-#include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/components/polling_component/polling_component.h"
+#include "esphome/components/uart/uart.h"
+#include "esphome/core/component.h"
 
+namespace esphome {
 namespace jsn_sensor {
 
-class JSNSensor : public esphome::PollingComponent {
+class JSNSensor : public Component {
  public:
-  JSNSensor(esphome::uart::UARTDevice *parent, esphome::sensor::Sensor *sensor)
-      : esphome::PollingComponent(500), uart_(parent), sensor_(sensor), buffer_pos_(0) {}
+  explicit JSNSensor(uart::UARTComponent *parent, sensor::Sensor *sensor)
+      : uart_(parent), sensor_(sensor) {}
 
-  void setup() override { buffer_pos_ = 0; }
+  void setup() override {
+    buffer_pos_ = 0;
+  }
 
-  void update() override {
+  void loop() override {
     uint8_t c;
     while (uart_->read_byte(&c)) {
-      if (c == 0x59) {  // Header
+      if (buffer_pos_ == 0 && c != 0xFF) continue; // header
+      if (buffer_pos_ == 0) {
         buffer_[0] = c;
         buffer_pos_ = 1;
       } else if (buffer_pos_ > 0) {
         buffer_[buffer_pos_++] = c;
-        if (buffer_pos_ == 9) {
-          uint16_t distance = buffer_[2] + (buffer_[3] << 8);
-          if (sensor_)
-            sensor_->publish_state(distance / 10.0f);  // mm -> cm
-          buffer_pos_ = 0;
-        }
+      }
+
+      if (buffer_pos_ == 4) { // 假設 4 bytes JSN-SR04T
+        int distance = (buffer_[2] << 8) | buffer_[3];
+        sensor_->publish_state(distance / 10.0f); // mm -> cm
+        buffer_pos_ = 0;
       }
     }
   }
 
  protected:
-  esphome::uart::UARTDevice *uart_;
-  esphome::sensor::Sensor *sensor_;
-  uint8_t buffer_[9];
+  uart::UARTComponent *uart_;
+  sensor::Sensor *sensor_;
+  uint8_t buffer_[4];
   uint8_t buffer_pos_;
 };
 
 }  // namespace jsn_sensor
+}  // namespace esphome
