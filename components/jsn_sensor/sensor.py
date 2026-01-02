6198f895f24ed7bc2679871bf8bc891b2e4e1e33
adolph0721 <garful.c@gmail.com>
2026-01-02 11:21:14 +0800

    Fix ESPHome external component folder structure

--- components/jsn_sensor/sensor.py
+++ components/jsn_sensor/sensor.py
@@ -9,16 +9,16 @@ JSNSensor = jsn_ns.class_("JSNSensor", cg.PollingComponent, sensor.Sensor)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-    cv.Required(sensor.CONF_ID): cv.declare_id(JSNSensor),
-    cv.Optional(sensor.CONF_NAME): cv.string,
-    cv.Optional(sensor.CONF_UNIT_OF_MEASUREMENT): cv.string,
-    cv.Optional(sensor.CONF_ICON): cv.icon,
-    cv.Optional(sensor.CONF_DEVICE_CLASS): sensor.DEVICE_CLASSES_SCHEMA,
-    cv.Optional(sensor.CONF_STATE_CLASS): sensor.STATE_CLASSES_SCHEMA,
-    cv.Optional(sensor.CONF_ACCURACY_DECIMALS): cv.positive_int,
-}).extend(sensor.SENSOR_SCHEMA)  # 把 sensor 基本選項加入 schema
+    cv.Required("id"): cv.declare_id(JSNSensor),
+    cv.Optional("name"): cv.string,
+    cv.Optional("unit_of_measurement"): cv.string,
+    cv.Optional("icon"): cv.string,
+    cv.Optional("device_class"): cv.string,
+    cv.Optional("state_class"): cv.string,
+    cv.Optional("accuracy_decimals"): cv.positive_int,
+}).extend(sensor.SENSOR_SCHEMA)  # 保留 sensor 標準選項
 
 async def to_code(config):
     uart_var = await cg.get_variable(config[CONF_UART_ID])
-    var = cg.new_Pvariable(config[sensor.CONF_ID], uart_var)
+    var = cg.new_Pvariable(config["id"], uart_var)
     await sensor.register_sensor(var, config)
