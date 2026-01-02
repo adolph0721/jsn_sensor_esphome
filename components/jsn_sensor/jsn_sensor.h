661ac2604360b325422677b935dde654767be11a
adolph0721 <garful.c@gmail.com>
2026-01-02 11:17:18 +0800

    Fix ESPHome external component folder structure

--- components/jsn_sensor/jsn_sensor.h
+++ components/jsn_sensor/jsn_sensor.h
@@ -1,7 +1,5 @@
 #pragma once
-
 #include "esphome.h"
-#include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/uart/uart.h"
 
@@ -11,18 +9,16 @@ class JSNSensor : public PollingComponent, public esphome::sensor::Sensor {
  public:
   JSNSensor(esphome::uart::UARTDevice *parent) : PollingComponent(500), uart_(parent) {}
 
-  void setup() override {
-    buffer_pos_ = 0;
-  }
+  void setup() override { buffer_pos_ = 0; }
 
   void update() override {
     uint8_t byte;
     while (uart_->read_byte(&byte)) {
       buffer_[buffer_pos_++] = byte;
-      if (buffer_pos_ == 4) {  // JSN-SR04T UART 4 bytes: 0xFF, high, low, checksum
+      if (buffer_pos_ == 4) {
         if (buffer_[0] == 0xFF) {
-          int distance = (buffer_[1] << 8) | buffer_[2];  // mm
-          publish_state(distance / 10.0f);  // 發佈 cm
+          int distance = (buffer_[1] << 8) | buffer_[2];
+          publish_state(distance / 10.0f);  // mm → cm
         }
         buffer_pos_ = 0;
       }
