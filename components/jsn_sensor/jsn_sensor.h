93d30bc0d56762494b2f2a6648cccd33f13012e9
adolph0721 <garful.c@gmail.com>
2026-01-02 10:06:14 +0800

    Fix ESPHome external component folder structure

--- components/jsn_sensor/jsn_sensor.h
+++ components/jsn_sensor/jsn_sensor.h
@@ -11,21 +11,24 @@ using namespace esphome::uart;
 
 class JSNSensor : public PollingComponent, public Sensor {
  public:
-  explicit JSNSensor(UARTComponent *parent) 
-      : PollingComponent(500), uart(parent) {}
+  explicit JSNSensor(UARTComponent *parent_uart) 
+      : PollingComponent(500), uart(parent_uart) {}
 
   void setup() override {
     buffer_pos = 0;
   }
 
   void update() override {
+    if (!uart) return;
+
     while (uart->available()) {
       uint8_t c = uart->read();
       buffer[buffer_pos++] = c;
 
       if (buffer_pos >= 4) {
+        // JSN-SR04T UART 回傳兩個 bytes 距離 (mm)
         uint16_t dist = (buffer[0] << 8) | buffer[1];
-        float d = dist / 10.0f;
+        float d = dist / 10.0f;  // 轉成 cm
         ESP_LOGD("jsn_sensor", "Distance raw=%u cm=%.1f", dist, d);
         this->publish_state(d);
         buffer_pos = 0;
@@ -34,7 +37,7 @@ class JSNSensor : public PollingComponent, public Sensor {
   }
 
  protected:
-  UARTComponent *uart;
+  uart::UARTComponent *uart;
   uint8_t buffer[4]{0};
   uint8_t buffer_pos{0};
 };
--- components/jsn_sensor/sensor.py
+++ components/jsn_sensor/sensor.py
@@ -1,38 +1,16 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, sensor
-from esphome.const import (
-    CONF_ID,
-    UNIT_CENTIMETER,
-    ICON_ARROW_EXPAND_VERTICAL,
-)
-
-CODEOWNERS = ["@adolph0721"]
-DEPENDENCIES = ["uart"]
+from esphome.components import sensor, uart
+from esphome import pins
 
 jsn_sensor_ns = cg.esphome_ns.namespace("jsn_sensor")
-JSNSensor = jsn_sensor_ns.class_(
-    "JSNSensor", cg.PollingComponent, sensor.Sensor
-)
+JSNSensor = jsn_sensor_ns.class_("JSNSensor", cg.PollingComponent, sensor.Sensor)
 
-CONFIG_SCHEMA = (
-    sensor.sensor_schema(
-        unit_of_measurement=UNIT_CENTIMETER,
-        icon=ICON_ARROW_EXPAND_VERTICAL,
-        accuracy_decimals=1,
-    )
-    .extend(
-        {
-            cv.GenerateID(): cv.declare_id(JSNSensor),
-            cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
-        }
-    )
-    .extend(cv.polling_component_schema("500ms"))
-)
+PLATFORM_SCHEMA = cv.PLATFORM_SCHEMA.extend({
+    cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
+}).extend(sensor.SENSOR_SCHEMA.schema)
 
 async def to_code(config):
-    uart_comp = await cg.get_variable(config["uart_id"])
-    var = cg.new_Pvariable(config[CONF_ID], uart_comp)
-
-    await cg.register_component(var, config)
+    uart_ = await cg.get_variable(config["uart_id"])
+    var = cg.new_Pvariable(config["name"], uart_)
     await sensor.register_sensor(var, config)
